b2783641af4ab5519ff88c18441434e7a7038a0a
Sidinei Silva <sidinei.silva02@gmail.com>
2020-07-08 17:00:52 -0300

    Incluindo arquivo de configuração do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,77 @@
 module.exports = {
   env: {
     es2020: true,
-    node: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint', 'prettier', 'eslint-plugin-import-helpers'],
   rules: {
+    'prettier/prettier': 'error',
+    'import/no-cycle': 'off',
+
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'no-await-in-loop': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.spec.ts', 'src/utils/tests/*.ts'],
+      },
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        // example configuration
+        newlinesBetween: 'always',
+        groups: [
+          '/^dotenv/',
+          '/^express/',
+          'module',
+          '/^@shared/',
+          ['sibling', '/module/', '/entity/', '/controller/', '/service/'],
+          ['parent', 'index'],
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
+
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+
+  settings: {
+    'import/extensions': ['.ts', '.js'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.js'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
