3906c03826f1c8ddf2ddec3535540320412be751
Sidinei Silva <sidinei.silva02@gmail.com>
2020-07-08 17:17:07 -0300

    Traduzindo yup

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import Controller from './controller';
 import ExceptionError from './exceptionError';
 import Module from './module';
+import YupTranslate from './yupTranslate';
 
-export { Module, Controller, ExceptionError };
+export { Module, Controller, ExceptionError, YupTranslate };
